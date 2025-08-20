8abc346b96f80e1d3788425492349baf6b065c29
Piotr Czarnecki <czarnecki.devv@gmail.com>
2025-08-20 13:07:23 +0200

    decorate views

--- toDoListAPI/toDoListAPIViews/views.py
+++ toDoListAPI/toDoListAPIViews/views.py
@@ -5,38 +5,47 @@ from rest_framework.decorators import api_view
 from db_model.models import User, List, Task
 from serializers import UserSerializer, ListSerializer, TaskSerializer
 
-from functools import wraps
+from decorators import get_post_data, validate_email
 
 
 @api_view(['POST'])
+@validate_email
+@get_post_data
 def signup(request):
     pass
 
 @api_view(['POST'])
+@get_post_data
 def login(request):
     pass
 
 @api_view(['POST'])
+@get_post_data
 def createList(request):
     pass
 
 @api_view(['POST'])
+@get_post_data
 def addToList(request):
     pass
 
 @api_view(['POST'])
+@get_post_data
 def updateItemFromList(request):
     pass
 
 @api_view(['POST'])
+@get_post_data
 def markDoneFromList(request):
     pass
 
 @api_view(['POST'])
+@get_post_data
 def deleteFromList(request):
     pass
 
 @api_view(['POST'])
+@get_post_data
 def deleteList(request):
     pass
 
