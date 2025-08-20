bdeab49a96d95f6a5974beec39ad5a7ca0dad35b
Piotr Czarnecki <czarnecki.devv@gmail.com>
2025-08-20 15:20:10 +0200

    fix/combine decorators into one for signup view
    Instead of using validate_email validate_password and checking parameters in view, all of this is done inside one decorator "check_signup_post_data"

--- toDoListAPI/toDoListAPIViews/decorators.py
+++ toDoListAPI/toDoListAPIViews/decorators.py
@@ -1,10 +1,36 @@
 from rest_framework.response import Response
 from rest_framework import status
-
 from functools import wraps
-from db_model.models import User, List, Task
+
+from db_model.models import User
+from toDoListAPI.settings import env
 
 import json
+
+# check input data from request body
+def check_signup_post_data(view):
+    @wraps(view)
+    def wrapper(request):
+        # check parameters
+        for param in ('name', 'email', 'password'):
+            if param not in request.data: # email and password fields were checked with decorators
+                return Response({'error', 'username, email or password is missing'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # check if email is unique
+        if User.objects.filter(email=request.data.get('email')).first() is not None:
+            return Response({'error', 'This email is already taken'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # validate password
+        if len(request.data.get('password')) > 50: # check length to not slow down the program with a for loop
+            return Response({'error', 'Password is too long'}, status=status.HTTP_400_BAD_REQUEST)
+
+        for i in request.data.get('password'):
+            if i not in json.loads(env('HASH_TABLE')).keys():
+                return Response({'error', 'Password contains forbidden symbols. Available symbols: a-z, A-Z, 0-9, !@#$%^&*()'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return view(request)
+
+    return wrapper
 
 # if requset body (data from API user) is wrapped in _content field, retrive it and assign to request.data
 def get_post_data(view):
@@ -19,20 +45,6 @@ def get_post_data(view):
 
         except (TypeError, ValueError):
             return Response({'error': 'Data was not sent'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        return view(request)
-
-    return wrapper
-
-# check if email is repeated
-def validate_email(view):
-    @wraps(view)
-    def wrapper(request):
-        if request.data.get('email') is None:
-            return Response({'error': 'Email is missing'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if User.objects.filter(email=request.data.get('email')).first() is not None:
-            return Response({'error': 'This email is taken'}, status=status.HTTP_400_BAD_REQUEST)
 
         return view(request)
 
